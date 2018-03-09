f9fe269340b987d8bd5ad3ff353b91fa27e810ac
Aaron Levin <awlevin@comcast.net>
2018-03-09 14:42:11 -0600

    added fixed times for texts to be sent to participants for the next week
    also moved participant information to its own config file

--- run.py
+++ run.py
@@ -1,35 +1,58 @@
-from flask import Flask, request, session
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import os, datetime, csv
+from twilio.rest import Client
+from datetime import datetime
+from threading import Timer
+import os, csv
+import json
 
+# Load environment configurations
 PORT = os.environ["PORT"]
 SECRET_KEY = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
 
+# Load participant details from config file (names and phone numbers)
+with open('participants.json') as f:
+    participants = json.load(f)
+
+# Configure Flask app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-participants = {
-    "+<country_code><phone_number>" : "<name_of_participant>"
-}
+"""
+Set up survey times
+"""
+def setup_survey_times():
+    now = datetime.now()
+    exec_dates = [datetime(2018, 3, day, hour=9) for day in range(10,15)]
+    delays = [(exec_date - now).seconds for exec_date in exec_dates]
+    timers = [Timer(delay, inquire_participants) for delay in delays]
+    [t.start() for t in timers]
 
-
+"""
+Texts the survey to all of the participants
+"""
 @app.route("/")
-def hello():
+def inquire_participants():
+    for cell_number, name in participants.items():
+        resp = MessagingResponse()
+        client = Client(TWILIO_ACCOUNT_SID, SECRET_KEY)
+        client.api.account.messages.create(
+        to=str(cell_number),
+        from_="+12242231260",
+        body="Hey " + name + "! How many times do you think you'll use the bus today? Please respond with 0-10. \n\nThanks again for helping out!")
     return "Hello World!"
 
+"""
+Handles all of the user's responses to the survey via webhook configured on Twilio
+"""
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """ RESPOND TO USER TEXT MESSAGES TO THE TWILIO NUMBER"""
-    counter = session.get('counter', 0)
-    counter = counter + 1
-    
     body = request.values.get('Body', None)
     
-    date = datetime.datetime.now().isoformat()
+    date = datetime.now().isoformat()
 
-    # save new counter value in the session
-    #session['counter'] = counter
-    
     from_number = request.values.get('From')
     if from_number in participants:
         name = participants[from_number]
@@ -39,29 +62,37 @@ def sms_reply():
     resp = MessagingResponse()
     
     if body.isdigit():
-        if (int(body) > 5 or int(body) < 0):
-            resp.message("Please enter a valid number between 0-5!")
+        if (int(body) > 10 or int(body) < 0):
+            log(date, body, name)
+            resp.message("Please enter a valid number between 0-10")
         else:
             resp.message("Thank you!")
             save_data(name, int(body), date)
     else:
-        resp.message("Please enter a valid number!")
-
-    """
-    # build the response
-    message = '{} has messaged {} {} times.' \
-        .format(name, request.values.get('To'), counter)
-    """
-
-    print(date)
+        log(date, body, name)
+        resp.message("Please enter a number between 0-10")
 
     return str(resp)
 
+"""
+Save the user's responses to a CSV file
+"""
 def save_data(name, value, timestamp):
     with open('data.csv', 'a') as csvfile:
         wr = csv.writer(csvfile, dialect='excel')
         wr.writerow([name, value, timestamp])
 
+"""
+If a response was invalid, this method will log what the user tried to input in their SMS
+"""
+def log(date, body, name):
+    with open('error_logs.txt', 'a') as logfile:
+        logfile.write(date + " --- " + name + " --- " + body + "\n")
+        
+
+
+
 if __name__ == "__main__":
+    setup_survey_times()
     app.run(debug=True, port=int(PORT))
     
